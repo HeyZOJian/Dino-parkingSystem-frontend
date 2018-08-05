7f8407af9ec229153bac2f366b149ac01ced9ed5
Jerry <jerry.zhang@oocl.com>
2018-08-05 18:17:11 +0800

    [Jerry]change workStatus of parkingBoy

--- src/reducers/employees.js
+++ src/reducers/employees.js
@@ -3,14 +3,7 @@ import ResourceAPI from '../api/ResourceAPI';
 export default (state = [], action) => {
     switch (action.type) {
       case 'GET_ALL_EMPLOYEES': {
-          const orders = [];
-          const length = action.orders.length
-          for(let i=0;i<length;i++){
-            orders[i]=action.orders[i];
-            orders[i].type=changeWords(action.orders[i].type)
-            orders[i].status=changeWords(action.orders[i].status)
-          }
-
+          console.log(action.employees);
         return action.employees;
       }
       case 'GET_SEARCH_EMPLOYEES':{
--- src/reducers/parkingboys.js
+++ src/reducers/parkingboys.js
@@ -1,13 +1,29 @@
 import ResourceAPI from '../api/ResourceAPI';
+import * as types from '../constant/constant'
 
 export default (state = [], action) => {
     switch (action.type) {
       case 'GET_ALL_PARKINGBOYS': {
           console.log(action.parkingBoys);
-        return action.parkingBoys;
+          const parkingBoys = [];
+          const length = action.parkingBoys.length
+          for(let i=0;i<length;i++){
+            parkingBoys[i]=action.parkingBoys[i];
+            parkingBoys[i].workStatus=changeWords(action.parkingBoys[i].workStatus)
+          }
+        return parkingBoys;
       }
       default:
         return state;
     }
   };
   
+  const changeWords=(keyWord)=>{
+    switch(keyWord){
+      case types.STATUS_ONDUTY:return "上班";
+      case types.STATUS_OFFDUTY:return "下班";
+
+      case types.STATUS_LATE:return "迟到";
+      case types.STATUS_LEAVE:return "请假";
+    }
+  }
